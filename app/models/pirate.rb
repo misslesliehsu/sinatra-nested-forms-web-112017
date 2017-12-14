f9475b064b95a3742ac24cb87bd199874d158d40
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-13 22:27:27 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,23 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+  @@all = []
+
+  def initialize(args)
+    #params = {"pirate"=>{"name"=>"Leslie", "weight"=>"350", "height"=>"6'5\"", "ships"=>[{"name"=>"a", "booty"=>"b", "type"=>"c"}, {"name"=>"d", "booty"=>"e", "type"=>"f"}]}}
+
+    @name = args[:pirate][:name]
+    @weight = args[:pirate][:weight]
+    @height = args[:pirate][:height]
+    @ships = args[:pirate][:ships]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+attr_accessor :name, :booty, :type
+
+@@all = []
+
+  def initialize(args)
+    #args =  {"name"=>"a", "booty"=>"b", "type"=>"c"}
+    @name = args[:name]
+    @booty = args[:booty]
+    @type = args[:type]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all = []
+  end
 end
